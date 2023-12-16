95128147ac2e7f572828aa40c13ae5b2e1f54c0d
ben <x_z_gfree@163.com>
2023-12-16 19:19:44 +0800

    feature: add document page.

---
+++ document/src/pages/index.tsx
@@ -0,0 +1,23 @@
+// pages/index.js
+
+import Head from "next/head";
+import { RecoilRoot } from "recoil";
+import AppContainer from "../components/app_container";
+import Content from "../components/content";
+import Navbar from "../components/navbar";
+import Sidebar from "../components/sidebar";
+
+export default function Home() {
+  return (
+    <RecoilRoot>
+      <Head>
+        <title>PhaseJS</title>
+      </Head>
+      <AppContainer>
+        <Navbar />
+        <Sidebar />
+        <Content />
+      </AppContainer>
+    </RecoilRoot>
+  );
+}
